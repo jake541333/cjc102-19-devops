0e05d42b98963301c0d2276f13e5fad07da75aaa
jake541333 <jake541333@gmail.com>
2025-12-26 13:10:34 +0800

    v6 online

--- web/app.py
+++ web/app.py
@@ -1,5 +1,5 @@
 """
-flask 第5版
+flask 第6版
 """
 
 from flask import Flask, request, abort
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def hello_world():
-    return 'This is version 5.'
+    return 'This is version 6.'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
